vlw/android_device_samsung_a3lte

init: Add vendor part of init lib

BoardConfig.mk
@@ -40,6 +40,9 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 10537124096
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
+# Init
+TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_a3.cpp
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_a3_defconfig
 

samsung-msm8916-caf/android_device_samsung_serranovelte

BoardConfig.mk
@@ -40,6 +40,9 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 10537124096
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
+# Init
+TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_serranovexx.cpp
+
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk